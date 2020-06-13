3281cede35fa788750bfbaaa6c37b6ed74ac8e08
sanjay <sanjay.bhambani@litmus7.com>
2020-06-13 22:08:39 +0530

    set mode

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,6 +7,7 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
+    mode: 'production',
     optimization: {
         minimizer:[new OptimizeCssAssetsPlugin(), new TerserPlugin()]
       },
